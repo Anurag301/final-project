5a36d54c7bd93eb7c36349247b77f3b4e19787f0
Anurag301 <115408889+Anurag301@users.noreply.github.com>
2022-11-26 19:27:29 +0530

    Add files via upload

---
+++ food_data.json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+{"1": {"name": "biriyani", "Quantity": 50, "Price": 2000, "Discount": 10, "Stock": 100}, "2": {"name": "pongal", "Quantity": 5, "Price": 1000, "Discount": 10, "Stock": 100}}
+
